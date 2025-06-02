93c2501890a3b2f5eb29aa7a26ce8664d54c055b
gsbingo17 <gsbingo@yeah.net>
2025-06-02 21:25:02 +0800

    enable SplitStageCountHint by setting it to 3L

--- v2/sourcedb-to-spanner/src/main/java/com/google/cloud/teleport/v2/source/reader/io/jdbc/iowrapper/JdbcIoWrapper.java
+++ v2/sourcedb-to-spanner/src/main/java/com/google/cloud/teleport/v2/source/reader/io/jdbc/iowrapper/JdbcIoWrapper.java
@@ -467,7 +467,7 @@ public final class JdbcIoWrapper implements IoWrapper {
              * Currently we mostly deal with auto incrementing keys, so we don't need a split depth to make the partition uniform, unless there is a large dataset with a lot of holes.
              * TODO(vardhanvthigle): if index is not of the type of a single auto incrementing key, don't set this.
              */
-            .setSplitStageCountHint(0L)
+            .setSplitStageCountHint(3L)
             .setDbParallelizationForSplitProcess(config.dbParallelizationForSplitProcess())
             .setDbParallelizationForReads(config.dbParallelizationForReads())
             .setAdditionalOperationsOnRanges(config.additionalOperationsOnRanges());
